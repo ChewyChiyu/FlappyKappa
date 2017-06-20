b4b2531c492d659908fd19c27af7f4c2be22767b
Evan Chen <evan@Evans-MacBook-Pro.local>
2017-06-20 14:58:13 -0700

    best game ever

--- FlappyKappa/GameScene.swift
+++ FlappyKappa/GameScene.swift
@@ -8,82 +8,205 @@
 
 import SpriteKit
 import GameplayKit
+import AVFoundation
+class GameScene: SKScene, SKPhysicsContactDelegate {
+    
+    var player = Kappa()
+    
+    var groundAlpha = SKSpriteNode()
+    var groundBeta = SKSpriteNode()
+    
+    var groundAnchorAlpha = CGPoint()
+    var groundAnchorBeta = CGPoint()
+    
+    var pipes = SKNode()
+    
+    var startGame:Bool = false
+    var isAlive:Bool = true
+    
+    var score:Int = 0
+    
+    var scoreLabel = SKLabelNode()
+    
+    var jumpSound: AVAudioPlayer?
+    var musicSound: AVAudioPlayer?
 
-class GameScene: SKScene {
-    
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
     override func didMove(to view: SKView) {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        player = (self.childNode(withName: "Kappa") as! Kappa)
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
+        groundAlpha = (self.childNode(withName: "GroundAlpha") as! SKSpriteNode)
+        groundBeta = (self.childNode(withName: "GroundBeta") as! SKSpriteNode)
+        groundAnchorAlpha = groundAlpha.position
+        groundAnchorBeta = groundBeta.position
+        
+        pipes = (self.childNode(withName: "Obstacles")!)
+        
+        scoreLabel = (self.childNode(withName: "ScoreLabel")! as! SKLabelNode)
+        scoreLabel.fontSize = 60
+        scoreLabel.fontName = "HelveticaNeue-Bold"
+        
+        physicsWorld.contactDelegate = self
+        //being music
+        playMusic()
+    }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        let pointA = contact.bodyA
+        let pointB = contact.bodyB
+        if(pointA.categoryBitMask == 1 && pointB.categoryBitMask == 8 || pointB.categoryBitMask == 1 && pointA.categoryBitMask == 8){
+            // kappa and target
+            score+=1
+        }else{
+            //kappa hit something else so died
+            isAlive = false
+            //death animation + impulse
+            player.physicsBody?.applyImpulse(CGVector(dx: -40, dy: 40))
+            player.zPosition  = 1
+            player.physicsBody?.collisionBitMask = 0
+            player.physicsBody?.contactTestBitMask = 0
             
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
+            
+            player.run(SKAction(named: "SpinOut")!,  completion: {
+                //restarting
+                self.stopMusic()
+                if let scene = GameScene(fileNamed:"GameScene") {
+                    let skView = self.view! as SKView
+                    
+                    skView.ignoresSiblingOrder = true
+                    
+                    scene.scaleMode = .aspectFill
+                    scene.size = skView.bounds.size
+                    
+                    
+                    
+                    skView.presentScene(scene, transition: SKTransition.fade(withDuration: 1.0))
+                }
+                
+            })
+            
+            
+            
+            
         }
+        return
     }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        startGame = true
+        if(startGame && isAlive){
+            player.physicsBody?.affectedByGravity = true
+            player.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+            player.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 65))
+            playJumpSound()
         }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+    func playJumpSound(){
+        
+        let path = Bundle.main.path(forResource: "smb_kick.wav", ofType:nil)!
+        let url = URL(fileURLWithPath: path)
+        
+        
+        do {
+            let sound = try AVAudioPlayer(contentsOf: url)
+            jumpSound = sound
+            sound.volume*=2
+            sound.play()
+        } catch {
+            // couldn't load file :(
+        }
+        
     }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    func playMusic(){
+        let path = Bundle.main.path(forResource: "Super Mario Bros. medley.mp3", ofType:nil)!
+        let url = URL(fileURLWithPath: path)
+        
+        do {
+            let sound = try AVAudioPlayer(contentsOf: url)
+            musicSound = sound
+            sound.numberOfLoops = -1 //chained forever
+            sound.play()
+        } catch {
+            // couldn't load file :(
+        }
     }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    func stopMusic(){
+        if musicSound != nil {
+            musicSound?.stop()
+            musicSound = nil
+        }
     }
-    
-    
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+        if(startGame && isAlive){
+            managePipes()
+            manageGround()
+            manageScore()
+        }
+    }
+    
+    func manageScore(){
+        scoreLabel.text = String(score)
+    }
+    func managePipes(){
+        //move pipes
+        
+        for pipe in pipes.children as! [SKReferenceNode] {
+            pipe.position.x-=2
+            //reset position of pipe if past
+            if(pipe.position.x <= -36){
+                //tamper with height
+                changeHeight(selected: pipe)
+                //shift back
+                pipe.position.x+=600
+                
+            }
+            
+        }
+        
+        
+        
+    }
+    func changeHeight(selected: SKReferenceNode){
+        //oops messed up on position in .sks files... gotta adapt to strange y position of -175.973 for center
+        let styleCenter = CGFloat(-175.973)
+        let styleHigh =  CGFloat(-175.973+100)
+        let styleLow =  CGFloat(-175.973-100)
+        
+        
+        switch(Int(arc4random_uniform(3))){
+        case 0:
+            selected.position.y = styleCenter
+            break
+        case 1:
+            selected.position.y = styleHigh
+            break
+        case 2:
+            selected.position.y = styleLow
+            break
+        default:
+            break
+        }
+        
+        
+    }
+    func manageGround(){
+        //moving both alpha and beta
+        groundAlpha.position.x-=2
+        groundBeta.position.x-=2
+        //exchange alpha and beta as they scroll
+        if(groundBeta.position.x <= groundAnchorAlpha.x){
+            groundAlpha.position.x = groundAnchorBeta.x
+            //switch groundAlpha and groundBeta
+            let tempGround = groundAlpha
+            groundAlpha = groundBeta
+            groundBeta = tempGround
+            //redo pipes
+            
+        }
+        
+        
     }
 }
---
+++ FlappyKappa/Kappa.swift
@@ -0,0 +1,14 @@
+//
+//  Kappa.swift
+//  FlappyKappa
+//
+//  Created by Evan Chen on 6/19/17.
+//  Copyright © 2017 Evan Chen. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class Kappa: SKSpriteNode {
+    
+}
